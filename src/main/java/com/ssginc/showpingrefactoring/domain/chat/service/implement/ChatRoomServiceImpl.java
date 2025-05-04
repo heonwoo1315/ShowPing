9819c1cdd8c7be8eabee383dd62372f5d2d3416d
HeonWoo Park <phwoo1315@naver.com>
2025-05-04 20:36:52 +0900

    Chore: 민호 도메인, 주일 도메인 domain 디렉토리 아래에 파일 위치 수정
    - 추가로 채팅 도메인과 신고 관리 도메인 프론트엔드 코드 파일위치도ㄷ 수정

--- src/main/java/com/ssginc/showpingrefactoring/domain/chat/service/implement/ChatRoomServiceImpl.java
+++ src/main/java/com/ssginc/showpingrefactoring/domain/chat/service/implement/ChatRoomServiceImpl.java
@@ -16,18 +16,18 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class ChatRoomServiceImpl implements ChatRoomService {
     private final ChatRoomRepository chatRoomRepository;
-    private final LiveRepository liveRepository; // 영상 정보 조회용
+    private final StreamRepository streamRepository; // 영상 정보 조회용
 
     /**
      * 스트림 번호와 최대 참가자 수를 기반으로 새 채팅방을 생성합니다.
      * @param streamNo 채팅방에 연결될 영상 번호
-//     * @param maxParticipants 최대 참가자 수
+    //     * @param maxParticipants 최대 참가자 수
      * @return 생성된 ChatRoom 객체
      */
     @Override
     public ChatRoom createChatRoom(Long streamNo ) {
         // 영상 엔티티 조회
-        Stream stream = liveRepository.findById(streamNo)
+        Stream stream = streamRepository.findById(streamNo)
                 .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 streamNo: " + streamNo));
 
         ChatRoom chatRoom = new ChatRoom();
