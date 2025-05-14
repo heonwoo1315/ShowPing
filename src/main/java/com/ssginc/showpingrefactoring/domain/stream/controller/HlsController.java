4b62d4092c94a26182e945b6c85db0e31c281d1b
Kim Daecheol <kdc9619@naver.com>
2025-05-14 17:43:43 +0900

    Feat: HLSController 클래스 Swagger 설정 구현

--- src/main/java/com/ssginc/showpingrefactoring/domain/stream/controller/HlsController.java
+++ src/main/java/com/ssginc/showpingrefactoring/domain/stream/controller/HlsController.java
@@ -1,6 +1,14 @@
 package com.ssginc.showpingrefactoring.domain.stream.controller;
 
 import com.ssginc.showpingrefactoring.domain.stream.service.HlsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -16,6 +24,7 @@ import reactor.core.publisher.Mono;
  * HLS와 관련한 요청-응답을 수행하는 컨트롤러 클래스
  * <p>
  */
+@Tag(name = "hls", description = "HLS 기반 VOD 재생")
 @Controller
 @RequestMapping("/api/hls")
 @RequiredArgsConstructor
@@ -29,6 +38,19 @@ public class HlsController {
      * @return HLS 파일이 포함된 응답객체 (확장자: m3u8)
      */
     @GetMapping(value = "/v1/{title}.m3u8")
+    @Operation(summary = "get M3U8 ver 1", description = "HLS metadata 요청 (생성 및 fetch)")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "성공",
+                    content = @Content(
+                            mediaType = "application/vnd.apple.mpegurl",
+                            schema = @Schema(type = "string", format = "binary")
+                    )
+            ),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
+    @Parameter(name = "title", description = "영상 제목", example = "노트북 특가!")
     public Mono<?> getHLSV1(@PathVariable String title) {
         // 불러온 m3u8 파일을 응답으로 전송
         return hlsService.getHLSV1(title)
@@ -45,6 +67,22 @@ public class HlsController {
      * @return TS 파일이 있는 응답객체 (확장자: ts)
      */
     @GetMapping(value = "/v1/{title}{segment}.ts")
+    @Operation(summary = "get TS ver 1", description = "10초 단위의 영상 segment 요청")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "성공",
+                    content = @Content(
+                            mediaType = "video/mp2t",
+                            schema = @Schema(type = "string", format = "binary")
+                    )
+            ),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
+    @Parameters ({
+            @Parameter(name = "title", description = "영상 제목", example = "노트북 특가!"),
+            @Parameter(name = "segment", description = "영상 segment 번호", example = "1")
+    })
     public Mono<?> getTsSegmentV1(@PathVariable String title,
                                   @PathVariable String segment) {
         // 불러온 ts 파일을 응답으로 전송
@@ -61,6 +99,19 @@ public class HlsController {
      * @return TS 파일이 있는 응답객체 (확장자: ts)
      */
     @GetMapping(value = "/v2/flux/{title}.m3u8")
+    @Operation(summary = "get M3U8 ver2 + WebFlux", description = "HLS metadata 요청 (fetch만 수행 + WebFlux)")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "성공",
+                    content = @Content(
+                            mediaType = "application/vnd.apple.mpegurl",
+                            schema = @Schema(type = "string", format = "binary")
+                    )
+            ),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
+    @Parameter(name = "title", description = "영상 제목", example = "노트북 특가!")
     public Mono<?> getHLSV2Flux(@PathVariable String title) {
         // 불러온 ts 파일을 응답으로 전송
         return hlsService.getHLSV2Flux(title)
@@ -77,6 +128,22 @@ public class HlsController {
      * @return TS 파일이 있는 응답객체 (확장자: ts)
      */
     @GetMapping(value = "/v2/flux/{title}{segment}.ts")
+    @Operation(summary = "get TS ver2 + WebFlux", description = "10초 단위의 영상 segment 요청 (WebFlux)")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "성공",
+                    content = @Content(
+                            mediaType = "video/mp2t",
+                            schema = @Schema(type = "string", format = "binary")
+                    )
+            ),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
+    @Parameters ({
+            @Parameter(name = "title", description = "영상 제목", example = "노트북 특가!"),
+            @Parameter(name = "segment", description = "영상 segment 번호", example = "1")
+    })
     public Mono<?> getTsSegmentV2Flux(@PathVariable String title,
                                       @PathVariable String segment) {
         // 불러온 ts 파일을 응답으로 전송
@@ -93,6 +160,19 @@ public class HlsController {
      * @return TS 파일을 포함한 응답 객체 (확장자: ts)
      */
     @GetMapping(value = "/v2/{title}.m3u8")
+    @Operation(summary = "get M3U8 ver2", description = "HLS metadata 요청 (fetch만 수행)")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "성공",
+                    content = @Content(
+                            mediaType = "application/vnd.apple.mpegurl",
+                            schema = @Schema(type = "string", format = "binary")
+                    )
+            ),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
+    @Parameter(name = "title", description = "영상 제목", example = "노트북 특가!")
     public ResponseEntity<?> getHLSV2(@PathVariable String title) {
         // 동기 방식으로 리소스를 가져옵니다.
         Resource resource = hlsService.getHLSV2(title);
@@ -112,6 +192,22 @@ public class HlsController {
      * @return TS 파일을 포함한 응답 객체 (확장자: ts)
      */
     @GetMapping(value = "/v2/{title}{segment}.ts")
+    @Operation(summary = "get TS ver2", description = "10초 단위의 영상 segment 요청")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "성공",
+                    content = @Content(
+                            mediaType = "video/mp2t",
+                            schema = @Schema(type = "string", format = "binary")
+                    )
+            ),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
+    @Parameters ({
+            @Parameter(name = "title", description = "영상 제목", example = "노트북 특가!"),
+            @Parameter(name = "segment", description = "영상 segment 번호", example = "1")
+    })
     public ResponseEntity<?> getTsSegmentV2(@PathVariable String title,
                                             @PathVariable String segment) {
         // 동기 방식으로 리소스를 가져옵니다.
