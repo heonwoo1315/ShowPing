744b7b5dd024bc61e53b47673f06a095e0bcd1ae
Kim Daecheol <kdc9619@naver.com>
2025-05-15 18:36:09 +0900

    Feat: BatchController Swagger 어노테이션 구현

--- src/main/java/com/ssginc/showpingrefactoring/common/controller/BatchController.java
+++ src/main/java/com/ssginc/showpingrefactoring/common/controller/BatchController.java
@@ -1,6 +1,14 @@
 package com.ssginc.showpingrefactoring.common.controller;
 
 import com.ssginc.showpingrefactoring.domain.stream.dto.request.VodTitleRequestDto;
+import com.ssginc.showpingrefactoring.domain.watch.dto.response.WatchResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
@@ -18,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  * 배치 작업을 처리하는 컨트롤러 클래스
  * <p>
  */
+@Tag(name = "batch", description = "Batch 작업 수행")
 @RestController
 @RequestMapping("/api/batch")
 @RequiredArgsConstructor
@@ -33,6 +42,29 @@ public class BatchController {
      * @return 작업 실행 ID를 포함한 ResponseEntity
      */
     @PostMapping("/hls/create")
+    @Operation(
+            summary = "HLS 생성 배치",
+            description = "영상 저장 후 HLS 생성을 위한 배치작업 진행"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "202",
+                    description = "HLS 생성을 위한 batch 작업 생성 완료"
+            ),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "영상 제목 정보 DTO",
+            required    = true,
+            content     = @Content(
+                    mediaType = "application/json",
+                    schema    = @Schema(
+                            type        = "object",
+                            description = "HLS 생성을 위한 영상 제목",
+                            example     = "{\"fileTitle\":\"stream_01_노트북_특가.mp4\"}"
+                    )
+            )
+    )
     public ResponseEntity<String> createHLS(@RequestBody VodTitleRequestDto vodTitleRequestDto) throws Exception {
         String title = vodTitleRequestDto.getFileTitle();
         JobParameters params = new JobParametersBuilder()
@@ -51,6 +83,29 @@ public class BatchController {
      * @return 작업 실행 ID를 포함한 ResponseEntity
      */
     @PostMapping("/subtitle/create")
+    @Operation(
+            summary = "자막 생성 배치",
+            description = "영상 저장 후 자막생성을 위한 배치작업 진행"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "202",
+                    description = "자막 생성을 위한 batch 작업 생성 완료"
+            ),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "자막 생성을 위한 영상 제목 정보 DTO",
+            required    = true,
+            content     = @Content(
+                    mediaType = "application/json",
+                    schema    = @Schema(
+                            type        = "object",
+                            description = "자막 생성을 위한 영상 제목",
+                            example     = "{\"fileTitle\":\"stream_01_노트북_특가.mp4\"}"
+                    )
+            )
+    )
     public ResponseEntity<String> createSubtitle(@RequestBody VodTitleRequestDto vodTitleRequestDto) throws Exception {
         String title = vodTitleRequestDto.getFileTitle();
         JobParameters params = new JobParametersBuilder()
