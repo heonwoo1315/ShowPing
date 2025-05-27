70952abd94d3fda7ce9be4883cd8d2c351425be1
Kim Daecheol <kdc9619@naver.com>
2025-05-27 16:46:55 +0900

    Feat: VOD 리스트에 필요한 요청변수 DTO 정의 #43

---
+++ src/main/java/com/ssginc/showpingrefactoring/domain/stream/dto/request/VodListRequestDto.java
@@ -0,0 +1,24 @@
+package com.ssginc.showpingrefactoring.domain.stream.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
+
+@Getter
+@Setter
+@Validated
+public class VodListRequestDto {
+
+    @Min(value = 0)
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+    private int pageNo;
+
+    @Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private Long categoryNo;
+
+    @Schema(allowableValues = {"mostView", "recent"}, requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private String sort = "recent";
+
+}
