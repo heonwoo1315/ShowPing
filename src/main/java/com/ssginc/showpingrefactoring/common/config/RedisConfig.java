78bb0bf5305adc33ab5a7128bbc67b2f9e65fb4e
HeonWoo Park <phwoo1315@naver.com>
2025-10-06 14:26:25 +0900

    feat: RedisConfig 설정 및 RedisTemplate Bean 등록
    - RedisConnectionFactory를 LettuceConnectionFactory로 구성하고, YML 파일에서 host, port, 비밀번호를 주입받아 설정
    - RedisTemplate<String, String> 및 StringRedisTemplate 빈을 등록하여 Redis 기반의 캐싱 및 토큰 관리 환경 구축
    - Key/Value Serializer로 StringRedisSerializer를 사용하여 문자열 기반 데이터 처리를 기본으로 설정

--- src/main/java/com/ssginc/showpingrefactoring/common/config/RedisConfig.java
+++ src/main/java/com/ssginc/showpingrefactoring/common/config/RedisConfig.java
@@ -1,10 +1,14 @@
 package com.ssginc.showpingrefactoring.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -12,12 +16,33 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableCaching
 public class RedisConfig {
 
+    // 💡 yml 설정 값 주입
+    @Value("${spring.data.redis.host}") private String redisHost;
+    @Value("${spring.data.redis.port}") private int redisPort;
+    @Value("${spring.data.redis.password}") private String redisPassword; // 💡 비밀번호 주입
+
+    // RedisConnectionFactory Bean 정의를 LettuceConnectionFactory로 변경
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        redisStandaloneConfiguration.setPassword(redisPassword); // 💡 비밀번호 설정
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
         return template;
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
     }
 }
