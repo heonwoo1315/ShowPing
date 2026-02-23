333d1fc6dc58c167b0746b0e78fd49df65be290c
HeonWoo Park <phwoo1315@naver.com>
2026-02-24 02:13:00 +0900

    chore: 타임존을 KST 한국 시간으로 고정

--- src/main/java/com/ssginc/showpingrefactoring/ShowPingRefactoringApplication.java
+++ src/main/java/com/ssginc/showpingrefactoring/ShowPingRefactoringApplication.java
@@ -1,10 +1,18 @@
 package com.ssginc.showpingrefactoring;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ShowPingRefactoringApplication {
+
+	@PostConstruct
+	public void started() {
+		// 애플리케이션 시작 시 타임존을 한국(KST)으로 고정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShowPingRefactoringApplication.class, args);
